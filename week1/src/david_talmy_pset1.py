8e09693f08357f2e5f900e741e452311e93c4825
dtalmy <dtalmy@utk.edu>
2022-04-01 13:25:46 -0400

    adding example pset solutions

--- week1/src/david_talmy_pset1.py
+++ week1/src/david_talmy_pset1.py
@@ -1,17 +1,25 @@
 import pylab as pl
 import numpy as np
 
-# setup figure
-py, ax = pl.subplots()
-ax.plot()
-pl.show()
-
 # setup time domain
 tmin,tmax = 0,10
 delta = 0.01
 nsteps = (tmax-tmin)/delta
-times = np.linspace(tmin,tmax,nsteps)
+times = np.linspace(tmin,tmax,int(nsteps))
 print('tmin =',tmin)
 print('tmax =',tmax)
 print('nsteps =', nsteps)
 
+# parameters
+P0 = 10000 # initial condition
+mu = 1 # growth rate
+Panalytical = P0*np.exp(mu*times)
+
+# figure
+f,ax=pl.subplots()
+ax.set_xlabel('Time (days)')
+ax.set_ylabel('Cells (ml$^{-1}$)')
+ax.plot(times,Panalytical,label='Analytical solution')
+pl.show()
+
+f.savefig('../figures/exponential_growth')
